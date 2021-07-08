15859730f34144aeb68d5605c77362c4118e423c
Roney-juma <ochirone10@gmail.com>
2021-07-08 15:24:38 +0300

    added the view to home page and the templates

--- movies/models.py
+++ movies/models.py
@@ -7,6 +7,11 @@ AGE_CHOICES=(
     ('Kids','Kids')
 )
 
+MOVIE_TYPE=(
+    ('single','Single'),
+    ('seasonal','Seasonal')
+)
+
 # Create your models here.
 class CustomUser(AbstractUser):
     profiles=models.ManyToManyField('Profile')
@@ -16,3 +21,18 @@ class Profile(models.Model):
     name=models.CharField(max_length=225)
     age_limit=models.CharField(max_length=5,choices=AGE_CHOICES)
     uuid=models.UUIDField(default=uuid.uuid4,unique=True)
+
+
+class Movie(models.Model):
+    title:str=models.CharField(max_length=225)
+    description:str=models.TextField()
+    created =models.DateTimeField(auto_now_add=True)
+    uuid=models.UUIDField(default=uuid.uuid4,unique=True)
+    type=models.CharField(max_length=10,choices=MOVIE_TYPE)
+    videos=models.ManyToManyField('Video')
+    flyer=models.ImageField(upload_to='flyers',blank=True,null=True)
+    age_limit=models.CharField(max_length=5,choices=AGE_CHOICES,blank=True,null=True)
+
+class Video(models.Model):
+    title:str = models.CharField(max_length=225,blank=True,null=True)
+    file=models.FileField(upload_to='movies')
